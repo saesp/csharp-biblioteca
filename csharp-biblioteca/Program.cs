b6b6331a2d02a8b8f274845e44a2cddf7b1d1f90
Salvatore Esposito <esposito.00@outlook.it>
2023-04-11 15:39:53 +0200

    created documents, books and dvd classes

--- csharp-biblioteca/Program.cs
+++ csharp-biblioteca/Program.cs
@@ -1,11 +1,14 @@
 ﻿//Si vuole progettare un sistema per la gestione di una biblioteca.
+
 //Gli utenti si possono registrare al sistema, fornendo:
 //cognome
 //nome
 //email
 //password
 //recapito telefonico
+
 //Gli utenti registrati possono prendere in prestito dei documenti che sono di vario tipo (libri, DVD).
+
 //I documenti sono caratterizzati da:
 //un codice identificativo di tipo stringa
 //titolo
@@ -13,7 +16,42 @@
 //settore (storia, matematica, economia, …)
 //uno scaffale in cui è posizionato
 //un autore (Nome, Cognome)
+
 //Per i libri si ha in aggiunta il numero di pagine, mentre per i dvd la durata.
+
 //L’utente deve poter eseguire delle ricerche per codice o per titolo e, eventualmente, prendere in prestito registrando il periodo (Dal/Al) del prestito e il documento.
 //Deve essere possibile effettuare la ricerca dei prestiti dato nome e cognome di un utente.
+
 //Creiamo anche una classe Biblioteca che contiene la lista dei documenti, la lista degli utenti e la lista dei prestiti. Contiene inoltre i metodi per le ricerche e per l’aggiunta dei documenti, utenti e prestiti.
+
+
+namespace library
+{
+    //superclasse
+    public class Documents
+    {
+        //attributi (private) + getter e setter
+        public string Code { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Sector { get; set; }
+        public string Shelf { get; set; }
+        public string Author { get; set; }
+    }
+
+    //classe figlia di Documents
+    class Books : Documents
+    {
+        public short PagesNumber { get; set; }
+    }
+
+    class Dvd : Documents
+    {
+        public int Duration { get; set; }
+    }
+}
+
+
+
+
+
